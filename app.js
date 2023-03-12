3dbbdcf5d69dfc48a842ce893085c7ef00952853
somayehbd <somayehbd@gmail.com>
2023-03-12 10:46:03 +0100

    ex01 (improved): create functions to calculate income, cost and balance.

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ let userAccount = { income: 0, cost: 0 }
 //income
 let AddIncome = function (userAccount, amount) {
     userAccount.income = userAccount.income + amount
-    console.log(`income:${userAccount.income} balance:${userAccount.income - userAccount.cost}`)
+    // console.log(`income:${userAccount.income} balance:${userAccount.income - userAccount.cost}`)
+    printBalance(userAccount, 1, amount);
 }
 
 // cost 
 let addCost = function (userAccount, amount) {
     userAccount.cost = userAccount.cost + amount
-    console.log(`addoutgo:${userAccount.cost} balance:${userAccount.income - userAccount.cost}`)
+    // console.log(`addoutgo:${userAccount.cost} balance:${userAccount.income - userAccount.cost}`)
+    printBalance(userAccount, 2, amount);
 }
-
+let printBalance=function(useraccount, operationtype, amount){
+    let balance=useraccount.income - useraccount.cost
+    let operationLabel = operationtype == 1 ? 'income' : 'cost';
+    console.log(`${operationLabel}: ${amount}, balance:${balance}`)
+}
 // Examples:
-AddIncome(userAccount, 2000)
-addCost(userAccount, 160)
-
-
-
+AddIncome(userAccount, 100)
+addCost(userAccount, 30)
