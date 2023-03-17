2a50f9f6b5932e472550eca9100b0a9945fa7c81
somayehbd <somayehbd@gmail.com>
2023-03-17 19:22:28 +0100

    ex08(improve):Added product search

--- DOM/ex08.js
+++ DOM/ex08.js
@@ -1,12 +1,29 @@
+
+
 document.querySelector('#Add-product-form').addEventListener('submit', function (e) {
     e.preventDefault();
     const productName = e.target.elements.ProductTitle.value;
     const productList = document.createElement('p')
     productList.textContent = productName
-    document.querySelector('#product-list').appendChild(productList)
-    console.log(productList)
+    document.querySelector('#product').appendChild(productList)
     e.target.elements.ProductTitle.value = ''
 })
 
 
+document.querySelector('#search-products').addEventListener('input', function (e) {
+    e.preventDefault()
+    const searchTitle = e.target.value.toLowerCase();
+    const productList = document.querySelectorAll('#product p');
+    productList.forEach(function (productNode) {
+        const title = productNode.textContent.toLowerCase();
+        if (!title.includes(searchTitle)) {
+           productNode.style.display='none'
+        } 
+        else{
+            productNode.style.display='block'
+        }
+    });
+
+
+})
 
